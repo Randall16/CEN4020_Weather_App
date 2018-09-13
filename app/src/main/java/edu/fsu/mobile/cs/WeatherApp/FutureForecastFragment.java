c8a352ae7b48d10a7276ecde8640e81ee39e5657
Randall <randallgr5@outlook.com>
2018-09-13 15:54:28 -0400

    Set up Tab Layout and created skeleton versions of Fragments.

---
+++ app/src/main/java/edu/fsu/mobile/cs/WeatherApp/FutureForecastFragment.java
@@ -0,0 +1,26 @@
+package edu.fsu.mobile.cs.WeatherApp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FutureForecastFragment extends Fragment {
+
+
+    public FutureForecastFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_future_forecast, container, false);
+    }
+
+}
--- app/src/main/java/edu/fsu/mobile/cs/WeatherApp/MainActivity.java
+++ app/src/main/java/edu/fsu/mobile/cs/WeatherApp/MainActivity.java
@@ -1,13 +1,46 @@
+/**
+ * Using a Tab Layout all credit goes to Android for their tutorial on how to set up a Tab Layout.
+ * https://www.androidhive.info/2015/09/android-material-design-working-with-tabs/
+ */
+
 package edu.fsu.mobile.cs.WeatherApp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CurrentForecastFragment(), "TODAY");
+        adapter.addFragment(new FutureForecastFragment(), "5 DAYS");
+        viewPager.setAdapter(adapter);
+    }
+
+
 }
